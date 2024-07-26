b30bae355d39231c74c5b76ce97417edcdee1223
Conrad Mo <51024076+conrad-mo@users.noreply.github.com>
2024-07-26 00:47:56 -0400

    Add manual override page + css fixes on scanner page (#4)
    * Add manual override page + css fixes on scanner page
    
    * Reset input field on page change
    
    * Forgot brackets oops
    
    * console.log cleanup
    
    * Update override message
    
    * Move toast to top

--- src/components/Manual_Override.tsx
+++ src/components/Manual_Override.tsx
@@ -1,22 +1,108 @@
-import { Button, Flex, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  Text,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+  useToast,
+} from "@chakra-ui/react";
+import axiosInstance from "../axiosInstance";
+import { SetStateAction, useState } from "react";
 
 interface OverrideProps {
   changePage: (pageNumber: number) => void;
 }
 
 export default function OverridePage({ changePage }: OverrideProps) {
+  const toast = useToast();
+  const [input, setInput] = useState("");
+  const [isError, setIsError] = useState(false);
+  const handleInputChange = (e: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    const value = e.target.value;
+    setInput(value);
+    if (value) {
+      setIsError(false);
+    }
+  };
+  const handleManualOverride = async () => {
+    if (input != "") {
+      try {
+        const response = await axiosInstance.post("/api/admin/walkin", {
+          email: input,
+        });
+        setInput("");
+        toast({
+          title: "Email link successfully sent",
+          position: "top",
+          status: "success",
+          isClosable: true,
+        });
+        console.log(response.data);
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (e: any) {
+        toast({
+          title: e.response.data.fallbackMessage || "An unknown error occured",
+          position: "top",
+          status: "error",
+          isClosable: true,
+        });
+      }
+    } else {
+      setIsError(true);
+    }
+  };
+  const handleBackButtonClick = () => {
+    setInput("");
+    changePage(0);
+  };
   return (
     <Flex
       style={{
         flexDirection: "column",
         alignItems: "center",
-        minHeight: "100vh",
-        margin: "16px",
-        gap: "24px",
+        justifyContent: "space-between",
+        height: "100svh",
+        marginTop: "16px",
+        marginLeft: "16px",
+        marginRight: "16px",
       }}
     >
-      <Text textAlign="center">Manual Override Page here</Text>
-      <Button width="50%" onClick={() => changePage(0)}>
+      <Flex
+        style={{
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "24px",
+          width: "100%",
+        }}
+      >
+        <Text textAlign="center">Manual Override Page</Text>
+        <FormControl isInvalid={isError}>
+          <FormLabel>Email</FormLabel>
+          <Input
+            type="email"
+            value={input}
+            onChange={handleInputChange}
+            width={"100%"}
+          />
+          {isError ? (
+            <FormErrorMessage>Email is required.</FormErrorMessage>
+          ) : (
+            <></>
+          )}
+        </FormControl>
+        <Button width="100%" onClick={() => handleManualOverride()}>
+          Submit
+        </Button>
+      </Flex>
+      <Button
+        width="100%"
+        marginBottom="32px"
+        onClick={() => handleBackButtonClick()}
+      >
         Back to scanner
       </Button>
     </Flex>
